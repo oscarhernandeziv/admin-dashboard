a27283a9be80e454186bb7935e95710f25f16299
oscarhernandeziv <oscarhernandeziv@gmail.com>
2024-09-01 19:40:35 -0700

    init commit

---
+++ migrations/0000_condemned_vanisher.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "users" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"email" text NOT NULL,
+	"username" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email"),
+	CONSTRAINT "users_username_unique" UNIQUE("username")
+);
